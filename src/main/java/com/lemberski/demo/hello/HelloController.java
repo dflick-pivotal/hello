967ce364b26fe6aca8497c54aca0c6a08fcbd050
Dieter Flick <dflick@flickd-a01.vmware.com>
2020-12-02 11:54:36 +0100

    changed config

--- src/main/java/com/lemberski/demo/hello/HelloController.java
+++ src/main/java/com/lemberski/demo/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public Mono<String> sayHello() {
-        return Mono.just("Hello World v3");
+        return Mono.just("Hello World v1");
     }
 
 }
